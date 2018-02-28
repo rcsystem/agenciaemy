bf941cc9e0457e8bb0640d0980a6145ace0ab00b
vicente <ing-vicente-dr@hotmail.com>
2018-02-28 15:35:52 -0600

    Cambio de menu

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -8,17 +8,19 @@
 	<meta http-equiv=”Cache-Control” content=”no-cache, mustrevalidate”>
 
 <!-- Link para los estilos CCS -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/normalize.css">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/normalize.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
 	<link href="https://file.myfontastic.com/2SQuVNdrs635VjAgGpanbP/icons.css" rel="stylesheet">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styles.css">
-	<link rel="shortcut icon" href="<?php echo base_url(); ?>icon/logo1_2_.ico" type="image/x-icon">
-	<link rel="icon" href="<?php echo base_url(); ?>icon/logo1_2_.ico" type="image/x-icon">
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="shortcut icon" href="icon/logo1_2_.ico" type="image/x-icon">
+	<link rel="icon" href="icon/logo1_2_.ico" type="image/x-icon">
 		<!-- Plugins -->
-		<link href="<?php echo base_url(); ?>assets/css/animations.css" rel="stylesheet">
+		<link href="assets/css/animations.css" rel="stylesheet">
 <!-- Custom css --> 
-		<link href="<?php echo base_url(); ?>assets/css/custom.css" rel="stylesheet">
+		<link href="assets/css/custom.css" rel="stylesheet">
+		<!--iconos online https://fontawesome.com/get-started-->
+		<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 
 
 	<title>EMY EDECANES Y MODELOS, S.A. DE C.V.</title>
@@ -32,21 +34,21 @@
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
 				<li class="sidebar-brand">
-				<a href="#"><center><img src="<?php echo base_url();?>assets/images/logo_1.png"  alt=""></center></a>				
+				<a href="#"><center><img src="assets/images/logo1 (2).png"  alt=""></center></a>				
 				</li>
                 <li>
-				<a href="<?php echo base_url();?>">Inicio</a>
+				<a href="#">Inicio</a>
 				
 				</li>
                 <li>
-				<a  data-toggle="collapse" href="#colla-1" role="button" aria-expanded="false" aria-controls="collapseExample">Quienes Somos</a>
+				<a  data-toggle="collapse" href="#colla-1" role="button" aria-expanded="false" aria-controls="collapseExample">Quienes Somos <i class="fas fa-chevron-down" style="font-size:12px;"></i></a>
 				<ul class="collapse list-unstyled" id="colla-1">
                 	<li><a href="#">Misión</a></li>
                     <li><a href="#">Visión</a></li>
                 </ul>
                 </li>
                 <li>
-				<a  data-toggle="collapse" href="#colla-2" role="button" aria-expanded="false" aria-controls="collapseExample">Edecanes</a>
+				<a  data-toggle="collapse" href="#colla-2" role="button" aria-expanded="false" aria-controls="collapseExample">Edecanes <i class="fas fa-chevron-down" style="font-size:12px;"></i></a>
 				<ul class="collapse list-unstyled" id="colla-2">
 					<li><a href="#">A</a></li>
             		<li><a href="#">AA</a></li>
@@ -54,14 +56,14 @@
 				</ul>
 				</li>
                 <li>
-				<a  data-toggle="collapse" href="#colla-3" role="button" aria-expanded="false" aria-controls="collapseExample">Animadores</a>
+				<a  data-toggle="collapse" href="#colla-3" role="button" aria-expanded="false" aria-controls="collapseExample">Animadores <i class="fas fa-chevron-down" style="font-size:12px;"></i></a>
 				<ul class="collapse list-unstyled" id="colla-3">
 					<li><a href="#">Mujer</a></li>
             		<li><a href="#">Hombre</a></li>
 				</ul>
                 </li>
                 <li>
-				<a  data-toggle="collapse" href="#colla-4" role="button" aria-expanded="false" aria-controls="collapseExample">Modelos</a>
+				<a  data-toggle="collapse" href="#colla-4" role="button" aria-expanded="false" aria-controls="collapseExample">Modelos <i class="fas fa-chevron-down" style="font-size:12px;"></i></a>
 				<ul class="collapse list-unstyled" id="colla-4">
 					<li><a href="#">Fotografia fija</a></li>
             		<li><a href="#">Pasarela</a></li>
@@ -70,7 +72,8 @@
                 <li>
                     <a href="#">Únete a nosostros</a>
                 </li>
-                <li>
+               
+			    <li>
                     <a href="contacto.php">Contacto</a>
                 </li>
             </ul>
@@ -83,7 +86,7 @@
         <div class="home-menu col-lg-12" >
 	
 
-			<div class="row">
+	<div class="row">
 					<div class="col-lg-2 boton-menu">
                     <!-- /#sidebar-wrapper -->
                     <div class="boton-menu-item">
@@ -91,18 +94,38 @@
 					</div>
 					</div>
 
-					<div class="col-lg-7" >
+					<div class="col-lg-5" >
 					<div class="home-logo">
 					<a href="index.php">
-						<img src="assets/images/logo-v.png" class="" alt="">
+						<img src="assets/images/logo-v.png" class="logo_chico" alt="">
 						</a>
 						</div>		
 					</div>
 
-					<div class="home-tel col-lg-3" >
-					
-		                    <label for="home-tel">E-mail: ventas@emy.mx</label>
-		                    <p> <a href="<?php echo base_url() ;?>index.php/inicio/registro">Registro</a> </p>
+					<div class="home-contacto col-lg-5">
+					<form>
+					<div class="form-row align-items-center" style="padding:18px 0px;">
+						<div class="col-auto" >
+							<input type="text" class="txt" id="inlineFormInput" placeholder="Correo Electronico">
+							<!--<p style=" padding:0px;"><a href="" style="color:#fff; font-size:8px;">¿Has olvidado los datos de la cuenta?</a></p>-->
+						</div>
+						<div class="col-auto" style="float: right;">
+							<input type="text" class="txt" id="inlineFormInputGroup" placeholder="Contraseña">
+							
+						</div>
+						<div class="col-auto">
+							<button type="submit" class="home-btn-login">Entrar</button>
+							
+						</div>
+						<div class="col-auto" style="padding:0px 0px;">
+							<div class="txt">
+								<label><a href="" style="color:#fff; font-size:15px;">Registrar</a></label>
+							</div>
+							
+						</div>
+					</div>
+					</form>
+		                   
 						<!-- <a href="#" class="btn btn-xl btn-light mr-4">Click Me!</a> -->
                     </div>
 
